0b30beb68563704ce402430dec25b67a15aa57f9
ChaunceyKiwi <chaunceykiwi@gmail.com>
2018-05-21 11:57:04 -0700

    Add binary insertion

--- Q2&Q3.cpp
+++ Q2&Q3.cpp
@@ -1,13 +1,52 @@
+/* Method 1 */
+/* Binary search + Two pointers */
+/* Add: O(logN) time */
+/* Find: O(N) time */
+/* O(N) space */
+
 class TwoSum {
 private:
   vector<int> nums;
 public:
+  /* Linear insertion */
   void add(int val) {
     vector<int>::iterator it = nums.begin();
     while (it < nums.end() && *it < val) {
       it++;
     }
     nums.insert(it, val);
+  }
+
+  /* Binary insertion */
+  void add(int target) {
+    int L = 0;
+    int R = (int)nums.size() - 1;
+    int index = 0;
+    
+    if (nums.size() == 0) {
+      nums.insert(nums.begin(), target);
+      return;
+    }
+    
+    while (L < R) {
+      int M = (L + R) / 2;
+      
+      if (target == nums[M]) {
+        index = M;
+        break;
+      }
+      
+      else if (target < nums[M]) {
+        R = M - 1;
+      }
+      
+      else if (target > nums[M]) {
+        L = M + 1;
+      }
+    }
+    
+    index = nums[L] < target ? L + 1 : L;
+    nums.insert(nums.begin() + index, target);
   }
 
   bool find(int target) {
